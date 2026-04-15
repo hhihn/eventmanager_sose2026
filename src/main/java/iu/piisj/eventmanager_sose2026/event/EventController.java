24cb098729ef772be70f16482fbeed7d5813a882
hhihn <heinkehihn@gmail.com>
2026-04-15 13:54:02 +0200

    implemented basic event controller

--- src/main/java/iu/piisj/eventmanager_sose2026/event/EventController.java
+++ src/main/java/iu/piisj/eventmanager_sose2026/event/EventController.java
@@ -1,4 +1,34 @@
 package iu.piisj.eventmanager_sose2026.event;
 
-public class EventController {
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+import java.io.Serializable;
+import java.util.List;
+
+// diese Annotation macht den Bean (Controller) in der View sichtbar. Dies passiert über die beans.xml dessen
+// Einstellungen
+@Named
+@ViewScoped
+public class EventController implements Serializable {
+
+    @Inject
+    private EventService eventService;
+
+    private List<Event> events;
+
+    @PostConstruct
+    public void init() {
+        events = eventService.getEvents();
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public List<String> getAvailableStatuses() {
+        return eventService.getAvailableStatuses();
+    }
 }
