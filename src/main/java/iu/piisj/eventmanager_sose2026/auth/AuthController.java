cf176c82a5c294fce6dbf4f9597f5c4791956a09
hhihn <heinkehihn@gmail.com>
2026-04-29 13:33:49 +0200

    login and logout in auth controller implemented

--- src/main/java/iu/piisj/eventmanager_sose2026/auth/AuthController.java
+++ src/main/java/iu/piisj/eventmanager_sose2026/auth/AuthController.java
@@ -1,11 +1,50 @@
 package iu.piisj.eventmanager_sose2026.auth;
 
 import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.context.ExternalContext;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
 import java.io.Serializable;
+import java.util.Optional;
 
 @Named
 @SessionScoped
 public class AuthController implements Serializable {
+
+    public static final String SESSION_USER_KEY = "AUTH_USER";
+
+    @Inject
+    private AuthService authService;
+
+    private SessionUser currentUser;
+
+    public boolean login(String userName, String password) {
+        Optional<SessionUser> sessionUser = authService.authenticate(userName, password);
+        if (sessionUser.isEmpty()){
+            return false;
+        }
+
+        currentUser = sessionUser.get();
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put(SESSION_USER_KEY, currentUser);
+        return true;
+    }
+
+    public String logout() {
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        externalContext.invalidateSession();
+        currentUser = null;
+        return "/login.xhtml?faces-redirect=true";
+    }
+
+    public boolean isLoggedIn() {
+        return currentUser != null;
+    }
+
+    public SessionUser getCurrentUser() {
+        return currentUser;
+    }
+
+
 }
