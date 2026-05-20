2fbb11523c31108ecf458115d15efdfc08198ea9
hhihn <heinkehihn@gmail.com>
2026-05-20 13:47:11 +0200

    event session dto for transfer from view to controller

---
+++ src/main/java/iu/piisj/eventmanager_sose2026/dto/EventSessionDTO.java
@@ -0,0 +1,71 @@
+package iu.piisj.eventmanager_sose2026.dto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class EventSessionDTO implements Serializable {
+
+    private String title;
+    private String speaker;
+    private String sessionType;
+    private String room;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSpeaker() {
+        return speaker;
+    }
+
+    public void setSpeaker(String speaker) {
+        this.speaker = speaker;
+    }
+
+    public String getSessionType() {
+        return sessionType;
+    }
+
+    public void setSessionType(String sessionType) {
+        this.sessionType = sessionType;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
